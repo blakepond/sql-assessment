31dbbacb83adab0fa6500c36e0df50e4477d0b29
Blake Pond <blakehp@icloud.com>
2021-11-12 12:26:59 -0700

    sql-assessment almost complete

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -1,6 +1,50 @@
+require("dotenv").config()
+const {CONNECTION_STRING} = process.env;
+const Sequelize = require("sequelize")
 
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect:"postgres",
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
 
 module.exports = {
+
+    getCountries: (req,res) => {
+        sequelize.query(`
+            SELECT * FROM countries;
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err=> console.log(err))
+    },
+    createCity: (req,res) => {
+        let {name,rating,countryId} = req.body
+        sequelize.query(`
+        INSERT INTO cities(name,rating,country_id)
+        VALUES('${name}',${rating},${countryId});
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err=> console.log(err))
+    },
+    getCities: (req,res) => {
+        sequelize.query(`SELECT ci.city_id, ci.name, ci.rating, country_id, ct.name
+        FROM cities AS ci, countries AS ct
+        JOIN ct.country_id = country_id
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+    deleteCity: (req,res) => {
+        const {id} = req.params
+        sequelize.query(`
+        DELETE FROM cities
+        WHERE city_id = ${id}
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+    },
     seed: (req, res) => {
         sequelize.query(`
             drop table if exists cities;
@@ -11,7 +55,12 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+            CREATE TABLE cities(
+                city_id SERIAL PRIMARY KEY,
+                name VARCHAR(20),
+                rating INT,
+                country_id INT NOT NULL REFERENCES countries(country_id)
+            );
 
             insert into countries (name)
             values ('Afghanistan'),
